2888ea3807838d35f17d62d74580a233edab7337
Alyeev <markmarkul123@gmail.com>
2021-05-01 16:13:40 +0400

    change port proxy

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,7 @@ export {default as Categories} from './categories'
 export {default as SortPoppup} from './SortPoppup'
 export {default as PizzaBlock} from './PizzaBlock'
 export {default as LoadingBlock} from './PizzaBlock/LoadingBlock'
+export {default as CartItem} from './cartItem'
 
 
 
